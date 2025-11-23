95af73c13bc591b57bc469db14f0266e477891cc
Xpert <ahmad.pc.saad@gmail.com>
2025-11-23 17:15:02 +0100

    box conf fix: Architecture bash

--- admin.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Wrapper script to run admin helper on VM
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -t vagrant@192.168.121.183 "sudo bash /opt/my-paas/admin_helper.sh"
